5caca7e6f84388cc39b2f7f13bd9151821fcd58b
aphirak sang-ngenchai <apiruk_sang-ngenchai@hotmail.com>
2014-05-03 00:41:00 +0700

    bavensky

--- Moisture_inthe_soil/Moisture_inthe_soil.ino
+++ Moisture_inthe_soil/Moisture_inthe_soil.ino
@@ -1,3 +1,13 @@
+  /*******************************************************************************
+   * Project  : Moisture                                                         *
+   * Compiler : Arduino 1.5.2                                                    *
+   * Board    : Arduino Pro mini 328                                             *
+   * Module   : Bluetooth Serial Module (HC-07 Slave mode)                       *
+   *          : Soil moisture detector module                                    *
+   * Author   : Bavensky :3                                                      *
+   * E-Mail   : Aphirak_Sang-ngenchai@hotmail.com                                *
+   * Date     : 03/05/2014 [dd/mm/yyyy]                                          *
+   *******************************************************************************/
   int moisture = 0;                // Variable moisture 
   
   void setup() 
